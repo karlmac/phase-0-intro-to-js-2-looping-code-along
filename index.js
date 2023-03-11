9f8a82c546bf1d1063ad07a074e61c05f9b1f24f
Karlmac Yerima <yerimakarlmac@gmail.com>
2023-03-11 03:08:09 -0600

    edits index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+//writeCards(["Charlie", "Samip", "Ali"], "birthday");
+//countDown(5);
+
+function writeCards(names, eventName)
+{
+    let messages = [];
+
+    for (let i=0; i < names.length; i++)
+    {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+
+        //console.log(messages);
+    }
+
+    return messages;
+}
+
+function countDown(num)
+{
+    let i = num;
+    while (i >= 0)
+    {
+        console.log(i);
+        i--;
+    }    
+}
